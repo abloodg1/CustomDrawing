c211557a4dc81e5706c562ed601c949ad134a31d
Adam Bloodgood <abloodg1@binghamton.edu>
2021-03-03 11:15:12 -0500

    Added new graphics, and functionality. Pacman can move and collect a dot

--- core/src/com/drawing/game/Drawing.java
+++ core/src/com/drawing/game/Drawing.java
@@ -6,17 +6,25 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 public class Drawing extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture dot;
 	Handler handler;
-	public float y;
-
+	float dotX;
+	float dotY;
+	Random r = new Random();
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Images/pacman.png");
+		dot = new Texture("Images/yellowDot.png");
+
+		dotX = r.nextFloat() * 1080;
+		dotY = r.nextFloat() * 1920;
 		handler = new Handler();
 		Gdx.input.setInputProcessor(handler);
 
@@ -26,16 +34,22 @@ public class Drawing extends ApplicationAdapter {
 	public void render () {
 		tick();
 
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, y, 64, 64);
+		batch.draw(img, Handler.x, Handler.y, 64, 64);
+		batch.draw(dot, dotX, dotY, 32, 32);
 		batch.end();
 
 	}
 
 	public void tick(){
-
+		Handler.x += Handler.speedX;
+		Handler.y += Handler.speedY;
+		if(Handler.x - 32 < dotX && dotX < Handler.x + 32 && Handler.y - 32 < dotY && dotY < Handler.y + 32){
+			dotX = r.nextFloat() * 1080;
+			dotY = r.nextFloat() * 1920;
+		}
 	}
 
 	@Override
--- core/src/com/drawing/game/Handler.java
+++ core/src/com/drawing/game/Handler.java
@@ -5,7 +5,11 @@ import com.badlogic.gdx.InputProcessor;
 
 public class Handler implements InputProcessor {
 
-    public float y = 0;
+    public static float speed = 7;
+    public static float speedX = speed;
+    public static float speedY = 0;
+    public static float x = 400;
+    public static float y = 400;
 
     @Override
     public boolean keyDown(int keycode) {
@@ -24,9 +28,20 @@ public class Handler implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        if (screenX < 64 && screenY < 64){
-            y = 100;
+        if(speedX == speed){
+            speedY = speed * -1;
+            speedX = 0;
+        }else if(speedY == speed * -1){
+            speedY = 0;
+            speedX = speed * -1;
+        }else if(speedX == speed * -1){
+            speedY = speed;
+            speedX = 0;
+        }else if(speedY == speed){
+            speedY = 0;
+            speedX = speed;
         }
+
         return false;
     }
 
